38110acbf9512bc150de4a2a0d8fbdf294e4c635
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-02-08 14:48:09 -0300

    Inserindo os registros.  "Fix error on issue #1 "

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>App Lista Tarefas</title>
+		<title>Lista Tarefas</title>
 
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -13,8 +13,8 @@
 		<nav class="navbar navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="#">
-					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-					App Lista Tarefas
+					<img src="img/log.png" width="30" height="30" class="d-inline-block align-top" alt="">
+					Lista Tarefas
 				</a>
 			</div>
 		</nav>
---
+++ tarefa_controller.php
@@ -0,0 +1,5 @@
+<?php
+
+    require_once '../../ListaTarefas/tarefa_controller.php'
+
+?>
